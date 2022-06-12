bcd925fe3b753b186329282fc3ff7f7e4d10f367
dmitry-vs <dmitryselin@inbox.ru>
2022-06-12 13:12:58 +0300

    feat: добавлены задачи из файла ramdaPureFunctions

---
+++ src/ramdaPureFunctions.ts
@@ -0,0 +1,32 @@
+import {
+  concat,
+  compose,
+  join,
+  last,
+  map,
+  prop,
+  sortBy,
+  toPairs,
+  split,
+  fromPairs,
+  drop,
+} from 'ramda';
+
+// Задание 1
+export type Team = { name: string; score: number };
+
+export const getTopName = compose(prop('name'), last, sortBy(prop('score')));
+
+// Задание 2
+export type QsObj = Record<string, string | number | boolean | object>;
+
+export const createQs = compose(
+  concat('?'),
+  join('&'),
+  map(([k, v]) => `${k}=${v}`),
+  toPairs
+);
+
+// Задание 3
+// @ts-ignore
+export const parseQs = compose(fromPairs, map(split('=')), split('&'), drop(1));
